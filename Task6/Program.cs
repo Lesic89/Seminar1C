076b4d129fa4c593c9ea27c7a80168246a0d3f88
lesic <lesic89@list.ru>
2022-11-20 22:50:21 +0400

    first commit

---
+++ Task6/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка)*/
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number % 2 == 1)
+{
+    Console.WriteLine ("Число" + " " + number + " " + "нечётное");
+}
+else
+{
+    Console.WriteLine ("Число" + " " + number + " " + "чётное");
+}
