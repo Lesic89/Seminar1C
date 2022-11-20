87c72e7d9214af095e76eefe5315698400f3b63d
lesic <lesic89@list.ru>
2022-11-20 22:35:49 +0400

    first commit

---
+++ Task4/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.*/
+
+Console.Write("Введите первое число: ");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int numberThree = Convert.ToInt32(Console.ReadLine());
+
+int max = numberOne;
+
+if (numberTwo > max)
+{
+    max = numberTwo;
+}
+if (numberThree > max)
+{
+    max = numberThree;
+}
+Console.WriteLine("Максимальное число ->" + " " + max);
+
+
