1a3a2d430af45e76ea6c2405af4dffadbcf3f11c
lesic <lesic89@list.ru>
2022-11-20 22:16:55 +0400

    first commit

---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее*/
+
+
+Console.Write("Введите первое число: ");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+
+if(numberOne > numberTwo)
+{
+Console.WriteLine(numberOne + " " + "больше чем" + " " + numberTwo );
+}
+else
+{
+Console.WriteLine(numberTwo + " " + "больше чем" + " " + numberOne);
+}
